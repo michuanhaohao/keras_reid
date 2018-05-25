42e1848824f7c2839c83837b0b1a3be6321b5ed5
michuanhaohao <haoluocsc@zju.edu.cn>
2018-05-25 20:35:49 +0800

    accelerate re_ranking using pytorch (gpu)

--- re_ranking_gpu.py
+++ re_ranking_gpu.py
@@ -27,6 +27,7 @@ from scipy.spatial.distance import cdist
 import torch
 
 def re_ranking(probFea, galFea, k1, k2, lambda_value):
+    # if feature vector is numpy, you shoudl use 'torch.tensor' transform it to tensor
     query_num = probFea.size(0)
     all_num = query_num + galFea.size(0)
     feat = torch.cat([probFea,galFea])
